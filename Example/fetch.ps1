c938943bb1a245192008fda4f769f749d5637da6
Alexander Wahlsten <alexander.wahlsten@sileon.com>
2023-03-22 11:35:01 +0100

    Added fetch ps1 and fetch.sh

--- Example/fetch.ps1
+++ Example/fetch.ps1
@@ -1,6 +1,6 @@
-wget -O Microsoft.Extensions.Logging.Abstractions.dll https://github.com/alexandersileon/pg-command/blob/main/Example/Microsoft.Extensions.Logging.Abstractions.dll?raw=true
-wget -O Npgsql.dll https://github.com/alexandersileon/pg-command/blob/main/Example/Npgsql.dll?raw=true
-wget -O pg-command.deps.json https://github.com/alexandersileon/pg-command/blob/main/Example/pg-command.deps.json?raw=true
-wget -O pg-command.runtimeconfig.json https://github.com/alexandersileon/pg-command/blob/main/Example/pg-command.runtimeconfig.json?raw=true
-wget -O pg-command.exe https://github.com/alexandersileon/pg-command/blob/main/Example/pg-command.exe?raw=true
-wget -O pg-command.dll https://github.com/alexandersileon/pg-command/blob/main/Example/pg-command.dll?raw=true
+Start-BitsTransfer -Destination Microsoft.Extensions.Logging.Abstractions.dll -Source https://github.com/alexandersileon/pg-command/blob/main/Example/Microsoft.Extensions.Logging.Abstractions.dll?raw=true
+Start-BitsTransfer -Destination Npgsql.dll -Source https://github.com/alexandersileon/pg-command/blob/main/Example/Npgsql.dll?raw=true
+Start-BitsTransfer -Destination pg-command.deps.json -Source https://github.com/alexandersileon/pg-command/blob/main/Example/pg-command.deps.json?raw=true
+Start-BitsTransfer -Destination pg-command.runtimeconfig.json -Source https://github.com/alexandersileon/pg-command/blob/main/Example/pg-command.runtimeconfig.json?raw=true
+Start-BitsTransfer -Destination pg-command.exe -Source https://github.com/alexandersileon/pg-command/blob/main/Example/pg-command.exe?raw=true
+Start-BitsTransfer -Destination pg-command.dll -Source https://github.com/alexandersileon/pg-command/blob/main/Example/pg-command.dll?raw=true
